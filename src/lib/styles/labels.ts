2bec4cfaf6da05055f9ce81d0bc29cf5d89205e9
WDever <alsrbtkfkd7@gmail.com>
2019-05-21 23:52:50 +0900

    Add useLocalVar hooks and labels styles and svgs

--- src/lib/styles/labels.ts
+++ src/lib/styles/labels.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const WrongLabel = styled.label`
   width: 27rem;
+  /* height: 3.125rem; */
   color: #ff0000;
   font-family: 'Noto Sans KR';
   font-size: 1rem;
