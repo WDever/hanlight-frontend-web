fe556e22c8aad18a5398d0e585d5805ada615ab2
WDever <alsrbtkfkd7@gmail.com>
2019-05-30 22:14:54 +0900

    Remove PrivateRoute component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.scss';
-import PrivateRoute from 'components/privateRoute';
 import PhoneCheckPage from 'pages/auth/phoneCheck';
 import LoginPage from 'pages/auth/login';
 import RegisterPage from 'pages/auth/register';
 import MainPage from 'pages/main';
 import IdFindPage from 'pages/auth/idFind';
 import PwFindPage from 'pages/auth/pwFind';
-// import RouteComponent from './Route';
+import NoticePage from 'container/noticePage';
 
 const App: React.FC = () => (
   <div id="app">
-    {/* <RouteComponent /> */}
     <Switch>
       <Route exact path="/auth/register/create" component={RegisterPage} />
       <Route exact path="/auth/idFind" component={IdFindPage} />
       <Route exact path="/auth/pwFind" component={PwFindPage} />
       <Route exact path="/auth/register" component={PhoneCheckPage} />
       <Route exact path="/auth" component={LoginPage} />
+      <Route exact path="/notice" component={NoticePage} />
       <Route exact path="/" component={MainPage} />
-      {/* <PrivateRoute exact path="/" enter={!!localStorage.getItem('accessToken')} redirectLocation="/auth" component={MainPage} /> */}
       <Redirect to="/" />
     </Switch>
   </div>
