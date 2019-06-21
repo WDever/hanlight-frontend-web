3bc70b78cd51d01010f83bc059402e0d283cdf47
WDever <alsrbtkfkd7@gmail.com>
2019-06-21 17:51:14 +0900

    Add BoardCommentContainer

--- src/container/board/comment/boardComment.container.tsx
+++ src/container/board/comment/boardComment.container.tsx
@@ -1,7 +1,57 @@
 import BoardCommentComponent from 'components/board/comment';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
+import {
+  AppState,
+  boardActions,
+  boardReducerActions,
+  DeleteBoardCommentParams,
+  LikeParams,
+  PatchBoardCommentParams,
+  ReportParams,
+} from 'store';
 
-const BoardCommentContainer = connect()(BoardCommentComponent);
+export interface BoardCommentProps {
+  deleteBoardCommentStatus: 'none' | 'pending' | 'success' | 'failure';
+  patchBoardCommentStatus: 'none' | 'pending' | 'success' | 'failure';
+}
+
+export interface BoardCommentMethod {
+  deleteBoardCommemnt(data: DeleteBoardCommentParams): void;
+  patchBoardCommemnt(data: PatchBoardCommentParams): void;
+}
+
+export interface BoardCommentOwnProps {
+  comments: Comment[];
+  commentCount: number;
+  boardPk: number;
+  report(data: ReportParams): void;
+  like(data: LikeParams): void;
+}
+
+const mapStateToProps = (
+  { board }: AppState,
+  ownProps: BoardCommentOwnProps,
+) => ({
+  deleteBoardCommentStatus: board.deleteBoardCommentStatus,
+  patchBoardCommentStatus: board.patchBoardCommentStatus,
+  ...ownProps,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<boardReducerActions>) => ({
+  deleteBoardCommemnt: bindActionCreators(
+    boardActions.deleteBoardCommemnt,
+    dispatch,
+  ),
+  patchBoardCommemnt: bindActionCreators(
+    boardActions.patchBoardCommemnt,
+    dispatch,
+  ),
+});
+
+const BoardCommentContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(BoardCommentComponent);
 
 export default BoardCommentContainer;
