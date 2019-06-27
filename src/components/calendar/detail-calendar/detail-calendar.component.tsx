f3dc34695259f4b8e4ca5b1bfc5c85f4c6426c9b
WDever <alsrbtkfkd7@gmail.com>
2019-06-27 21:35:32 +0900

    ㅁ

--- src/components/calendar/detail-calendar/detail-calendar.component.tsx
+++ src/components/calendar/detail-calendar/detail-calendar.component.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 
+import CalendarItem from 'components/calendar/calendarItem';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
 const DetailCalendarComponent: React.FC = () => {
-  return (<div />);
-}
+  return (
+    <Wrapper>
+    </Wrapper>
+  );
+};
 
 export default DetailCalendarComponent;
---
+++ src/container/calendar/detail-calendar/detail-calendar.container.tsx
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { AppState, calendarActions, calendarReducerActions } from 'store';
+
+export interface DetailCalendarProps {}
+
+export interface DetailCalendarMethod {}
