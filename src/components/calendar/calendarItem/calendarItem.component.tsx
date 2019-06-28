efc65f7fb684a7fe45fcb3462cd8e42ab4903f2c
WDever <alsrbtkfkd7@gmail.com>
2019-06-28 15:06:41 +0900

    a

--- src/components/calendar/calendarItem/calendarItem.component.tsx
+++ src/components/calendar/calendarItem/calendarItem.component.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 interface BoxProps {
   today: boolean;
+  type: 'main' | 'detail';
 }
 
 interface TextProps {
@@ -17,8 +18,8 @@ interface ItemProps extends BoxProps {
 }
 
 const Box = styled.div<BoxProps>`
-  width: 15.875rem;
-  height: 15.875rem;
+  width: ${props => (props.type === 'main' ? '15.875rem' : '12.5rem')};
+  height: ${props => (props.type === 'main' ? '15.875rem' : '16.575rem')};
   box-shadow: ${props =>
     props.today ? '0 6px 30px 0 rgba(139, 139, 139, 0.16)' : 'none'};
   border: ${props => (props.today ? 'none' : 'solid 1px #b9b9b9')};
@@ -28,6 +29,8 @@ const Box = styled.div<BoxProps>`
   align-items: center;
   justify-content: center;
   border-radius: 1rem;
+  margin: ${props => (props.type === 'detail' ? '2.1875rem 1rem' : 'none')};
+  /* margin-bottom: ${props => (props.type === 'detail' ? '2.1875rem' : 'none')}; */
 `;
 
 const ContentsWrapper = styled.div`
@@ -63,8 +66,9 @@ const CalendarItem: React.FC<ItemProps> = ({
   day,
   contents,
   today,
+  type,
 }) => (
-  <Box today={today}>
+  <Box type={type} today={today}>
     <ContentsWrapper>
       <DateWrapper>
         <Texts>{today ? '오늘은' : '다가오는'}</Texts>
--- src/components/calendar/detail-calendar/detail-calendar.component.tsx
+++ src/components/calendar/detail-calendar/detail-calendar.component.tsx
@@ -1,15 +1,140 @@
 import * as React from 'react';
 
 import CalendarItem from 'components/calendar/calendarItem';
+import {
+  DetailCalendarMethod,
+  DetailCalendarProps,
+} from 'container/calendar/detail-calendar';
+import moment from 'moment';
+import 'moment/locale/ko';
 import styled from 'styled-components';
 
+const { useEffect, useState } = React;
+
 const Wrapper = styled.div`
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  max-width: 81rem;
+  width: 90%;
+  min-height: 100%;
 `;
 
-const DetailCalendarComponent: React.FC = () => {
+const TitleBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  margin-top: 3rem;
+
+  span {
+    font-family: 'yg-jalnan';
+    font-size: 2.25rem;
+  }
+`;
+
+const MonthBar = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  margin-right: 2rem;
+  margin-bottom: 0.5rem;
+  align-items: flex-end;
+
+  span {
+    font-family: 'yg-jalnan';
+    font-size: 1.125rem;
+  }
+`;
+
+const MonthItem = styled.span<{ now: boolean }>`
+  color: ${props => (props.now ? '#4470ff' : '#a9a9a9')};
+  cursor: pointer;
+`;
+
+const CalendarWrapper = styled.div`
+  width: 100%;
+  /* display: flex;
+  justify-content: space-between;
+  flex-flow: row wrap; */
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+const CalendarLine = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DetailCalendarComponent: React.FC<
+  DetailCalendarMethod & DetailCalendarProps
+> = ({ getCalendar, getCalendarStatus, accessToken, calendar }) => {
+  const [selectedMonth, setSelecetedMonth] = useState<string>(
+    moment().format('M'),
+  );
+  const monthList = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+  ];
+
+  const MonthList = monthList.map((item, i) => {
+    return (
+      <MonthItem
+        key={i}
+        now={item === selectedMonth}
+        onClick={() => setSelecetedMonth(item)}
+      >
+        {item}월
+      </MonthItem>
+    );
+  });
+
+  const CalendarList =
+    getCalendarStatus === 'success'
+      ? calendar.map((item, i) => {
+          const today =
+            moment().format('M.D') === `${selectedMonth}.${item.date}`;
+
+          return (
+            <CalendarItem
+              year={moment().format('YYYY')}
+              month={selectedMonth}
+              day={item.date}
+              contents={item.detail}
+              today={today}
+              key={i}
+              type="detail"
+            />
+          );
+        })
+      : [];
+
+  useEffect(() => {
+    getCalendar({
+      accessToken,
+      month: selectedMonth,
+      year: moment().format('YYYY'),
+    });
+  }, [selectedMonth]);
+
   return (
     <Wrapper>
+      <TitleBar>
+        <span>학사일정</span>
+        <MonthBar>{MonthList}</MonthBar>
+      </TitleBar>
+      <CalendarWrapper>{CalendarList}</CalendarWrapper>
     </Wrapper>
   );
 };
