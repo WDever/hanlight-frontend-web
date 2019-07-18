bb524bfe9e4a868fbe61a509dd336f54c4fbb8f0
WDever <alsrbtkfkd7@gmail.com>
2019-07-18 10:33:44 +0900

    feat: Hanseithon redux

--- src/store/action/hanseithon.action.ts
+++ src/store/action/hanseithon.action.ts
@@ -3,15 +3,27 @@ import { Action } from 'redux';
 import {
   ActiveReportData,
   Board,
+  Category,
   Comment,
   ErrorResponse,
+  GetTeamResTeyp,
   ModalTypes,
+  PostTeamParams,
+  PostTeamResType,
 } from 'store/model';
 import { createStandardAction } from 'typesafe-actions';
 
 export const DEEM = 'DEEM';
 
 export const MODAL = 'MODAL';
+
+export const GET_TEAM = 'GET_TEAM';
+export const GET_TEAM_SUCCESS = 'GET_TEAM_SUCCESS';
+export const GET_TEAM_FAILURE = 'GET_TEAM_FAILURE';
+
+export const POST_TEAM = 'POST_TEAM';
+export const POST_TEAM_SUCCESS = 'POST_TEAM_SUCCESS';
+export const POST_TEAM_FAILURE = 'POST_TEAM_FAILURE';
 
 export class Deem implements Action {
   public readonly type = DEEM;
@@ -25,9 +37,51 @@ export class Modal implements Action {
   public constructor(public payload: ModalTypes) {}
 }
 
+export class GetTeam implements Action {
+  public readonly type = GET_TEAM;
+
+  public constructor(public payload: Category) {}
+}
+
+export class GetTeamSuccess implements Action {
+  public readonly type = GET_TEAM_SUCCESS;
+
+  public constructor(public payload: GetTeamResTeyp) {}
+}
+
+export class GetTeamFailure implements Action {
+  public readonly type = GET_TEAM_FAILURE;
+
+  public constructor(public payload: ErrorResponse) {}
+}
+
+export class PostTeam implements Action {
+  public readonly type = POST_TEAM;
+
+  public constructor(public payload: PostTeamParams) {}
+}
+
+export class PostTeamSuccess implements Action {
+  public readonly type = POST_TEAM_SUCCESS;
+
+  public constructor(public payload: PostTeamResType) {}
+}
+
+export class PostTeamFailure implements Action {
+  public readonly type = POST_TEAM_FAILURE;
+
+  public constructor(public payload: ErrorResponse) {}
+}
+
 export const hanseithonActions = {
   deem: createStandardAction(DEEM)<boolean>(),
   modal: createStandardAction(MODAL)<ModalTypes>(),
+  getTeam: createStandardAction(GET_TEAM)<Category>(),
 };
 
-export type hanseithonReducerActions = Deem | Modal;
+export type hanseithonReducerActions =
+  | Deem
+  | Modal
+  | GetTeam
+  | GetTeamSuccess
+  | GetTeamFailure;
--- src/store/model/hanseithon.model.ts
+++ src/store/model/hanseithon.model.ts
@@ -1,5 +1,40 @@
 export type ModalTypes = 'none' | 'create' | 'join' | 'match' | 'join-success' | 'create-success';
 
+export type Category = 'l' | 'g';
+
+export interface TeamMemberType {
+  name: string;
+  leader: string;
+  studuntId: string;
+  position: string;
+}
+
+export interface TeamType {
+  pk: number;
+  name: string;
+  leaderName: string;
+  category: Category;
+  createAt: string;
+
+  teamMember: TeamMemberType[];
+}
+
+export interface PostTeamParams {
+  category: Category;
+  teamName: string;
+  userPosiotion: string;
+}
+
+export interface PostTeamResType {
+  team: TeamType;
+  code: number;
+  createdAt: string;
+}
+
+export interface GetTeamResTeyp {
+  team: TeamType[];
+}
+
 export interface HanseithonModel {
   deemStatus: boolean;
   modalType: ModalTypes;
