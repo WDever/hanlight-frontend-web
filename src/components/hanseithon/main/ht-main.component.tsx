a97ae59113b54f867bbcf3744af8c06cb7ead502
Nods <taxci79@gmail.com>
2019-07-18 16:58:44 +0900

    fixed: ht-main mobileL media query

--- src/components/hanseithon/main/ht-main.component.tsx
+++ src/components/hanseithon/main/ht-main.component.tsx
@@ -14,6 +14,10 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+
+  @media ${Device.tabletS} {
+    align-items: center;
+  }
 `;
 
 const TitleWrapper = styled.div`
