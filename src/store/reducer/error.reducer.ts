fb99e4f0d12060c729e3e73ac364a01bf9c5a8ff
WDever <alsrbtkfkd7@gmail.com>
2019-07-21 17:44:26 +0900

    fixed: Restore error reducer

--- src/store/reducer/error.reducer.ts
+++ src/store/reducer/error.reducer.ts
@@ -17,37 +17,23 @@ export const errorReducer = (
   produce(state, draft => {
     switch (action.type) {
       case 'SET_ERROR':
-        console.log(action.payload);
-        console.log(action.payload.err);
-        console.log(action.payload.err.response);
-        // if (action.payload.err.response) {
-        if (action.payload.err) {
-          if (action.payload.err.response) {
-            draft.onError += 1;
-            draft.code = action.payload.err.response.data.code;
-            draft.message = action.payload.err.response.data.message;
-            draft.name = action.payload.err.response.data.name;
-            draft.time = moment().unix();
-          } else if (
-            action.payload.err.stack === 'Request failed with status code 401'
-          ) {
-            draft.name = 'Token Error';
-            draft.onError += 1;
-            draft.time = moment().unix();
-            draft.code = 401;
-            draft.message = '토큰이 만료되었거나 잘못되었습니다.';
-          } else if (
-            !state.time ||
-            state.code ||
-            moment().unix() - state.time >= 5
-          ) {
-            draft.onError += 1;
-            draft.code = 0;
-            draft.message =
-              '네트워크 상태가 좋지 않습니다. 새로고침하여 요청을 완료해주세요.';
-            draft.name = 'Network Error';
-            draft.time = moment().unix();
-          }
+        if (action.payload.err.response) {
+          draft.onError += 1;
+          draft.code = action.payload.err.response.data.code;
+          draft.message = action.payload.err.response.data.message;
+          draft.name = action.payload.err.response.data.name;
+          draft.time = moment().unix();
+        } else if (
+          !state.time ||
+          state.code ||
+          moment().unix() - state.time >= 5
+        ) {
+          draft.onError += 1;
+          draft.code = 0;
+          draft.message =
+            '네트워크 상태가 좋지 않습니다. 새로고침하여 요청을 완료해주세요.';
+          draft.name = 'Network Error';
+          draft.time = moment().unix();
         }
         break;
 
