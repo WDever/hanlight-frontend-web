6f1ac48efde0081b8e849eae503b5d9e035cfaf8
WDever <alsrbtkfkd7@gmail.com>
2019-07-24 10:08:22 +0900

    feat: Timer working

--- src/components/hanseithon/main/timer/ht-timer.component.tsx
+++ src/components/hanseithon/main/timer/ht-timer.component.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 import { Device } from 'lib/styles';
+import moment from 'moment';
+import 'moment/locale/ko';
 import styled from 'styled-components';
+
+const { useState, useEffect } = React;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -14,7 +18,11 @@ const Wrapper = styled.div`
   font-weight: bold;
   color: #ffffff;
 
-  margin-bottom: 37.75rem;
+  margin-bottom: 39.75rem;
+
+  @media ${Device.laptop} {
+    margin-bottom: 32rem;
+  }
 
   @media ${Device.mobileL} {
     font-size: 1.5rem;
@@ -22,8 +30,38 @@ const Wrapper = styled.div`
   }
 `;
 
+const minUnit = 60;
+const hourUnit = minUnit * 60;
+const day = hourUnit * 24;
+const totalTime = 21 * hourUnit + 30 * minUnit;
+
 const HTTimerComponent: React.FC = () => {
-  return <Wrapper>04시 : 15분 : 32초</Wrapper>;
+  const [sec, setSec] = useState<string>('00');
+  const [min, setMin] = useState<string>('00');
+  const [hour, setHour] = useState<string>('00');
+
+  const formatTwoDigit = (x: number): string => ('0' + x).slice(-2);
+
+  const computeTime = () => {
+    const inHour = moment().get('hour') * 3600;
+    const inMin = moment().get('minute') * 60;
+    const inSec = moment().get('second');
+
+    const sum = inHour + inMin + inSec;
+
+    const SumTime = sum >= totalTime ? day + totalTime - sum : totalTime - sum;
+
+    setHour(formatTwoDigit(Math.floor(SumTime / 3600)));
+    setMin(formatTwoDigit(Math.floor((SumTime % 3600) / 60)));
+    setSec(formatTwoDigit(Math.floor(SumTime % 60)));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => computeTime(), 1000);
+
+  })
+
+  return <Wrapper>{hour}시 : {min}분 : {sec}초</Wrapper>;
 };
 
 export default HTTimerComponent;
