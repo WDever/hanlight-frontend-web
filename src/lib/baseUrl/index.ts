298cfdfa9dcc806e4d55df99f14e433a778aa7a4
WDever <alsrbtkfkd7@gmail.com>
2019-08-05 23:43:33 +0900

    feat: Fix baseUrl according to NODE_ENV

--- src/lib/baseUrl/index.ts
+++ src/lib/baseUrl/index.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://test.hanlight.kr/',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'https://test.hanlight.kr/'
+      : 'https://api.hanlight.kr/',
 });
