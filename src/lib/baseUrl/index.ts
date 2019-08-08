02b7f01e198128f79700f85dd3e19f8abd7ffe1c
WDever <alsrbtkfkd7@gmail.com>
2019-08-08 16:11:43 +0900

    feat: Add custom environment variables and divide baseUrl by custom environment variables

--- src/lib/baseUrl/index.ts
+++ src/lib/baseUrl/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development'
+    process.env.REACT_APP_CUSTOM_NODE_ENV === 'development'
       ? 'https://test-backend.hanlight.kr/'
       : 'https://api.hanlight.kr/',
 });
